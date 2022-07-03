34522dab4f77dffdc63ef2b0499eefee19af78b5
suhavni <suhavni@gmail.com>
2022-07-03 18:57:03 +0700

    added constraints to db tab;es

--- models/booking.go
+++ models/booking.go
@@ -13,7 +13,7 @@ Booking JSON Object
 */
 
 type Booking struct {
-	ID      int64        `json:"id" gorm:"primary_key"`
+	ID      int64        `json:"id" gorm:"primary_key;autoincrement;not null"`
 	Booking pgtype.JSONB `gorm:"type:jsonb;default:'[]';not null"`
 	User    User         `gorm:"foreignkey:UserID"`
 	UserID  int64        `json:"user_id" gorm:"not null"`
--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	Id             int    `json:"id" gorm:"primaryKey"`
-	Username       string `json:"username"`
-	HashedPassword string `json:"hashed_password"`
-	Email          string `json:"email"`
+	Id             int    `json:"id" gorm:"primaryKey;autoincrement;not null"`
+	Username       string `json:"username" gorm:"not null;unique"`
+	HashedPassword string `json:"hashed_password" gorm:"not null"`
+	Email          string `json:"email" gorm:"not null;unique"`
 	JWTPrivateKey  string `json:"jwt_private_key"`
 }
