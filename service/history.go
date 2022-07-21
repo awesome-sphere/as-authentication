8b2ff52ab19bc6f062b1ec76f852ddd841bf362c
Vanessa5331 <earth5331@gmail.com>
2022-07-22 02:47:41 +0700

    Update input format for get history

--- service/history.go
+++ service/history.go
@@ -6,29 +6,27 @@ import (
 	"github.com/awesome-sphere/as-authentication/db"
 	"github.com/awesome-sphere/as-authentication/db/models"
 	"github.com/awesome-sphere/as-authentication/jwt"
-	"github.com/awesome-sphere/as-authentication/serializer"
 	"github.com/gin-gonic/gin"
 )
 
 func GetHistory(c *gin.Context) {
-	authorized, _ := jwt.AuthorizeToken(c)
+	authorized, claims := jwt.AuthorizeToken(c)
 	if !authorized {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"message": "Unauthorized. Invalid or expired token.",
 		})
 		return
 	}
-	var input serializer.HistorySerializer
-	if err := c.BindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": err.Error(),
-		})
-		return
+
+	user_id, ok := claims["user_id"].(float64)
+
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "User ID is not a number."})
 	}
 
 	var booking models.Booking
 
-	if err := db.DB.First(&booking, "user_id", input.UserID).Error; err != nil {
+	if err := db.DB.FirstOrInit(&booking, "user_id", user_id).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
@@ -36,6 +34,6 @@ func GetHistory(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"history": booking,
+		"history": booking.Booking,
 	})
 }
